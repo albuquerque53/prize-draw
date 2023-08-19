d8432efbc832f52a2dd118cad835a81cb587a97c
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-08-19 19:33:45 -0300

    feat: adding prize draw page

---
+++ resources/views/livewire/prize-draw.blade.php
@@ -0,0 +1,11 @@
+<div class="text-center">
+
+    <h1 class="rounded">
+        {{ $winner }}
+    </h1>
+
+    <button class="rounded border-2 border-blue-400 p-2 hover:bg-blue-400" wire:click="raffle">
+        Raffle!
+    </button>
+
+</div>
