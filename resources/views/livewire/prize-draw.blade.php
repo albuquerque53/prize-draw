4bb8eb723ab8d2e7107772b5e38400cf5ded16eb
Gabriel Albuquerque <albuquerque.se@proton.me>
2023-08-19 21:20:30 -0300

    chore: to add a default message before raffle

--- resources/views/livewire/prize-draw.blade.php
+++ resources/views/livewire/prize-draw.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-col items-center justify-center h-screen">
 
     <div wire:stream="winner" class="rounded m-10 h-20">
-        {{ $winner?->nickname }}
+        {{ $winner?->nickname ?? 'Click in raffle button' }}
     </div>
 
     <button class="rounded border-2 border-blue-400 p-2 hover:bg-blue-400" wire:click="raffle">
